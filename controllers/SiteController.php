72beffe45cff4ac4a57db22dcfd7e11debdd7caa
RVasiliy <vasiliy-next@rambler.ru>
2018-01-23 23:00:41 +0100

    Added logout action

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,9 +4,39 @@ namespace app\controllers;
 
 use Yii;
 use app\models\RegisterForm;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
 use yii\web\Controller;
 
 class SiteController extends Controller {
+
+    public function behaviors() {
+        return [
+            'access' =>[
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'register'],
+                'rules' => [
+                    [
+                        'actions' => ['register'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' =>[
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+    
     public function actions() {
         return [
             'error' => [
@@ -31,5 +61,11 @@ class SiteController extends Controller {
         }
 
         return $this->render('register', ['model' => $model]);
+    }
+
+    public function actionLogout() {
+        Yii::$app->getUser()->logout();
+
+        return $this->goHome();
     }
 }
